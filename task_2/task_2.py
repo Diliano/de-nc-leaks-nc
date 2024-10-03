8315ab8ae3d0628468df266e5ec96343ca288968
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-10-03 11:59:25 +0100

    create and add functionality for the get_interests() function, which retrieves usernames from northcoders.json and then makes a request to the api for each username, storing interests of each into a list and then writing the results to the interests.json file

---
+++ task_2/task_2.py
@@ -0,0 +1,30 @@
+import requests
+import json
+from pprint import pprint
+
+"""
+Write a function called `get_interests` that uses the newly found usernames for each northcoder to retrieve information on everyone's interests. This function should:
+
+1. Read the `northcoders.json` file you created in task 1
+2. For every person, use their `username` and make a request to `https://nc-leaks.herokuapp.com/api/people/:username/interests` to get their interests.
+3. Each response will be a JSON with a person key. Collect up the data on this `person` key into a list.
+4. Once you have all responses in the list, save it to a file called `interests.json`.
+"""
+
+def get_interests():
+    with open("task_1/northcoders.json") as f:
+        data = json.load(f)
+
+    results = []
+
+    for person in data:
+        username = person["username"]
+        url = f"https://nc-leaks.herokuapp.com/api/people/{username}/interests"
+        response = requests.get(url)
+        formatted_response = response.json()
+        results.append(formatted_response["person"])
+
+    with open("task_2/interests.json", "w") as f:
+        f.write(json.dumps(results, indent=4))
+
+# get_interests()
